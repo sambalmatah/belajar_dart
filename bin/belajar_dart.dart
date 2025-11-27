5fa601ed81def53eef422c75164d56df23710382
sambalmatah <yudiarinugroho14@gmail.com>
2025-11-27 14:56:35 +0700

    pertemuan 01 membuat proyek baru

---
+++ bin/belajar_dart.dart
@@ -0,0 +1,8 @@
+// import belajar_dart dari folder lib
+import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+
+// buat void main baru yang menampilkan Hello World dan interpolation .calculate
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart.calculate()}!');
+
+}
