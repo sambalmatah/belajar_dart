346c8cf3f90d09291f231c923f838d6d20a933e0
sambalmatah <yudiarinugroho14@gmail.com>
2025-12-08 12:36:57 +0700

    pertemuan 17 while and do while

---
+++ bin/17.while_and_do_while.dart
@@ -0,0 +1,18 @@
+void main() {
+  // 1. while
+  var i = 1;
+
+  while (i <= 100) {
+    print(i);
+    i++;
+  }
+
+
+  // 2. do while
+  var j = 1;
+
+  do {
+    print(j);
+    j++;
+  } while (j <= 100);
+}
